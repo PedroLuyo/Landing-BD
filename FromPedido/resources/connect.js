57d403dd9021b208347a73714f50a3802421ecb8
PedroPorrasLuyo <pedro.porras@vallegrande.edu.pe>
2022-06-20 08:38:42 -0500

    pre

---
+++ FromPedido/resources/connect.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var mysql = require("mysql");
+var app = express();
+var cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+var conexion = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "admin",
+    database: "dbrestaurante",
+  });
+
+  conexion.connect(function (error) {
+    if (error) {
+      throw error;
+    } else {
+      console.log("Conexión exitosa");
+    }
+  });
+
+  const puerto = process.env.PUERTO || 3000;
+
+app.listen(puerto, function () {
+  console.log("Servidor funcionando en puerto: " + puerto);
+});
+
+app.post("/api/pedido", (req, res) => {
+	let data = {
+    	usercon: req.body.USERCON,
+    	emauscon: req.body.EMAUSCON,
+    	msgcon: req.body.MSGCON,
+	};
+	let sql = "INSERT INTO pedido SET ?";
+	conexion.query(sql, data, function (error, results) {
+  	if (error) {
+    	throw error;
+  	} else {
+    	console.log(data);
+    	res.send(data);
+  	}
+	});
+  });
---
+++ FromPedido/resources/registro.js
@@ -0,0 +1,50 @@
+const url = "http://localhost:3000/api/pedido/";
+let resultados = '';
+const formArticulo = document.querySelector("form");
+const usercon = document.getElementById("USERCON");
+const emauscon = document.getElementById("EMAUSCON");
+const msgcon = document.getElementById("MSGCON");
+var opcion = '';
+
+btnCrear.addEventListener('click', () => {
+    console.log("Acción de listar activada");
+    opcion = 'crear';
+});
+
+formArticulo.addEventListener('submit',
+    (e) => {
+   	 e.preventDefault();
+   	 if (opcion == 'crear') {
+   		 if (USERCON.value == "" || EMAUSCON.value == "" || MSGCON.value == "") {
+       		 alert("Asegúrese de que todos los campos estén completos");
+       		 return false;
+   		 } else {
+       		 console.log("Todos los campos están completos");
+       		 fetch(
+           		 url,
+           		 {
+               		 method: 'POST',
+               		 headers: {
+                   		 'content-Type':'application/json'
+               		 },
+               		 body: JSON.stringify(
+                   		 {
+                       		 USERCON: USERCON.value,
+                       		 EMAUSCON: EMAUSCON.value,
+                       		 MSGCON: MSGCON.value
+                   		 }
+               		 )
+           		 }
+       		 )
+       		 .then(
+           		 response => response.json()
+       		 )
+       		 .then(
+           		 response => location.reload()
+       		 );
+   		 }
+   	 } else if(opcion == 'editar'){
+   		 console.log("Activado el ");
+   	 }
+    }
+);
